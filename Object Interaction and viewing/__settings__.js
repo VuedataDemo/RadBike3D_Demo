a8fec08db676578a6d42fd855ff5544be4bdd210
DevdenSolutions <68840254+DevdenSolutions@users.noreply.github.com>
2022-06-23 16:38:53 +0530

    initial commit

---
+++ Object Interaction and viewing/__settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1438730.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 86332039, 86332613, 89159425, 90356678, 90357902 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
