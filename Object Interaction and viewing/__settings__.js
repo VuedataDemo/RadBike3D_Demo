36263684ede21e7c882062415665dbdf0e929c65
RamKumarS07 <68840254+DevdenSolutions@users.noreply.github.com>
2022-06-25 16:42:16 +0530

    Modified

--- Object Interaction and viewing/__settings__.js
+++ Object Interaction and viewing/__settings__.js
@@ -1,6 +1,6 @@
 ASSET_PREFIX = "";
 SCRIPT_PREFIX = "";
-SCENE_PATH = "1438730.json";
+SCENE_PATH = "1455923.json";
 CONTEXT_OPTIONS = {
     'antialias': true,
     'alpha': false,
@@ -8,7 +8,7 @@ CONTEXT_OPTIONS = {
     'preferWebGl2': true,
     'powerPreference': "default"
 };
-SCRIPTS = [ 86332039, 86332613, 89159425, 90356678, 90357902 ];
+SCRIPTS = [ 90557264, 90557335, 90557358, 90557441, 90558278, 90558313, 90570542, 90629801, 90629803 ];
 CONFIG_FILENAME = "config.json";
 INPUT_SETTINGS = {
     useKeyboard: true,
@@ -18,4 +18,6 @@ INPUT_SETTINGS = {
 };
 pc.script.legacy = false;
 PRELOAD_MODULES = [
+    {'moduleName' : 'DracoDecoderModule', 'glueUrl' : 'files/assets/90629803/1/draco.js', 'wasmUrl' : 'files/assets/90629802/1/draco.wasm.wasm', 'fallbackUrl' : 'files/assets/90629801/1/draco.wasm.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/90612203/1/basis.wasm.js', 'wasmUrl' : 'files/assets/90612204/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/90612202/1/basis.js', 'preload' : false},
 ];
